6e5247ec7686a0061c7f4fe3867685c6aab66fef
Merket <mcp2mx@gmail.com>
2025-12-20 20:25:24 +0100

    First commit

---
+++ asteroid-z/playerController.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed := 600.0
+
+
+func _process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft")
+	input_vector.y = Input.get_action_strength("moveDown") - Input.get_action_strength("moveUp")
+
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * speed
+	
+	move_and_slide()
